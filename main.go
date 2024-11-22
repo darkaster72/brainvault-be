e7da65f9978a9ddb7c95b4151990c5f631654789
Ankit Sharma <sankit72@gmail.com>
2024-11-22 14:47:22 +0530

    add path to recieve the parsed article

--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/pocketbase/pocketbase"
-    "github.com/pocketbase/pocketbase/apis"
-    "github.com/pocketbase/pocketbase/core"
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-    app := pocketbase.New()
+	app := pocketbase.New()
 
-    // serves static files from the provided public dir (if exists)
-    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-        e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
-        return nil
-    })
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		// for accepting the parsed content
+		e.Router.POST("/api/brainvault/artices/:id/content", func(c echo.Context) error {
+			id := c.PathParam("id")
+			return c.JSON(http.StatusOK, map[string]string{"message": "Hello " + id})
+		}, apis.RequireAdminAuth())
+		return nil
+	})
 
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+	app.OnModelAfterCreate("articles").Add(func(e *core.ModelEvent) error {
+		log.Println(e.Model.TableName())
+		log.Println(e.Model.GetId())
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
