8017e35e2950f420ac7c5d72d350534d2d98cf21
Ankit Sharma <sankit72@gmail.com>
2024-11-26 17:18:17 +0530

    update content flow

--- main.go
+++ main.go
@@ -22,7 +22,36 @@ func main() {
 		// for accepting the parsed content
 		e.Router.POST("/api/brainvault/artices/:id/content", func(c echo.Context) error {
 			id := c.PathParam("id")
-			return c.JSON(http.StatusOK, map[string]string{"message": "Hello " + id})
+
+			type Article struct {
+				Title         string `json:"title"`
+				Content       string `json:"content"`
+				TextContent   string `json:"textContent"`
+				Length        int    `json:"length"`
+				Excerpt       string `json:"excerpt"`
+				Byline        string `json:"byline"`
+				Dir           string `json:"dir"`
+				SiteName      string `json:"siteName"`
+				Lang          string `json:"lang"`
+				PublishedTime string `json:"publishedTime"`
+			}
+
+			var body Article
+			if err := json.NewDecoder(c.Request().Body).Decode(&body); err != nil {
+				return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request body"})
+			}
+			record, _ := app.Dao().FindRecordById("articles", id)
+			record.Set("content", body.Content)
+			record.Set("excerpt", body.Excerpt)
+			record.Set("length", body.Length)
+			record.Set("byline", body.Byline)
+			record.Set("siteName", body.SiteName)
+			record.Set("lang", body.Lang)
+			record.Set("publishedTime", body.PublishedTime)
+
+			app.Dao().Save(record)
+
+			return c.JSON(http.StatusOK, map[string]string{"status": "success"})
 		}, apis.RequireAdminAuth())
 		return nil
 	})
