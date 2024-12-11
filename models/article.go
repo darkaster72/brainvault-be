dfb62b020de16ab4558ace4997c72cb35ead9723
Ankit Sharma <sankit72@gmail.com>
2024-12-11 21:31:20 +0530

    update model

--- models/article.go
+++ models/article.go
@@ -1,14 +1,21 @@
 package models
 
 type Article struct {
-	Title         string `json:"title"`
-	Content       string `json:"content"`
-	TextContent   string `json:"textContent"`
-	Length        int    `json:"length"`
-	Excerpt       string `json:"excerpt"`
-	Byline        string `json:"byline"`
-	Dir           string `json:"dir"`
-	SiteName      string `json:"siteName"`
-	Lang          string `json:"lang"`
-	PublishedTime string `json:"publishedTime"`
+	Byline         string `json:"byline"`
+	CollectionID   string `json:"collectionId"`
+	CollectionName string `json:"collectionName"`
+	ContentStatus  string `json:"content_status"`
+	Created        string `json:"created"`
+	Deleted        bool   `json:"deleted"`
+	Excerpt        string `json:"excerpt"`
+	ID             string `json:"id"`
+	Lang           string `json:"lang"`
+	Length         int64  `json:"length"`
+	PublishedTime  string `json:"publishedTime"`
+	SiteName       string `json:"siteName"`
+	Slug           string `json:"slug"`
+	Title          string `json:"title"`
+	Updated        string `json:"updated"`
+	URL            string `json:"url"`
+	Userid         string `json:"userid"`
 }
